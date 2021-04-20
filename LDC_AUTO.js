b723712bfae840ec31823111f8507844e7c5f850
Sup3rPlays <73548496+SPFck@users.noreply.github.com>
2021-04-20 12:59:37 +0100

    Add files via upload

---
+++ LDC_AUTO.js
@@ -0,0 +1,17 @@
+/*
+	LDC_AUTO.js Source Code
+ 	Written by @SPFck
+  
+ 	Made for https://longdogechallenge.com/
+*/
+
+const timer0 = ms => new Promise(res => setTimeout(res, ms))
+
+async function load () {
+	while(true) {
+  	injectWow(100);
+  	await timer0(0.25);
+  }
+}
+
+load();
